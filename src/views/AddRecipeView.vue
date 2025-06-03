0338e0e7e3f11ec0d9f2adc40285e60ce0a5e186
mezugatfukiko <bantuganjm@gmail.com>
2025-06-04 01:20:23 +0800

    added fade effect on add recipe success message

--- src/views/AddRecipeView.vue
+++ src/views/AddRecipeView.vue
@@ -327,3 +327,14 @@ function handleImageUpload(event: Event) {
     </div>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
